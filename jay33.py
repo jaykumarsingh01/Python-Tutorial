49b40f5363eddd867d3e0a7eea8b5161a4fe4728
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-28 11:47:19 +0530

    Add files via upload

---
+++ jay33.py
@@ -0,0 +1,32 @@
+dic= {
+    "jay":"human being",
+    "spoon":"object"
+   
+}
+print(dic["jay"])
+
+
+dic={
+    1:"jay kumar singh",
+    2:"arya singh ",
+    3:"aaryan singh",
+    4:"chirag choudhary",
+    5:"dhruv kumar singh",
+    6:"krish chouhan",
+    7:"krishna kr. jha",
+    8:"kunal tiwari",
+    9:"shashank toamr",
+    10:"niketa senger",
+    11:"prakhar kumar ",
+    13: "sherya kasana"
+
+}
+print(dic[int(input("enter your roll no."))])
+
+print(dic)
+
+info={'name': 'jay','age':19,'eligble':True}
+print(info)
+print(info['name'])
+print(info.get('name'))
+
