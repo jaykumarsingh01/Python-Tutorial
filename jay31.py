4e961a8aa31187d6ba172ba4d0c56b1dfb5bdf6f
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-28 11:46:35 +0530

    Add files via upload

---
+++ jay31.py
@@ -0,0 +1,12 @@
+s={2,2,2,2,2,2,2,2,2,2,2,2,22,2,22,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,4,6,7,8,9,10}
+print(s)
+
+j={"jay kuar sinh",19 ,False,5.9,19}
+print(j)
+
+# creatin a epty set
+jay=set()
+print(type(jay))
+
+for value in j:
+    print(value)  # This will raise a TypeError because you can't iterate over an empty set
