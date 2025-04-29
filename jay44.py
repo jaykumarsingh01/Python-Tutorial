b2b9c14ccf9101f57834c7d4fe3c0188af06e315
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-04-29 10:31:20 +0530

    Add files via upload

---
+++ jay44.py
@@ -0,0 +1,15 @@
+import math
+result= math.sqrt(9)
+print(result)
+
+from math import sqrt,pi
+result=sqrt(9)*pi
+print(result)
+
+
+import math as a
+result= a.sqrt(9)*a.pi
+print(result)
+
+import math
+print(dir(math))
