73a496acea78ad29ef8954eac6c6a6f7b30f11c1
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-05-13 04:41:59 +0530

    Add files via upload

---
+++ jay68.py
@@ -0,0 +1,6 @@
+# exercise 7
+
+# write a program to clear the cluster inside a folder on your compider.
+
+# 2) you should ure os module, to rename as the Png image from 1.png all the way till n.png 
+# where n is the number of png files in that  folder do the same for other file formate-
