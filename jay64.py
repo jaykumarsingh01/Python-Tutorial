58875285ae6060fd96da73f6bf0952a2de197569
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-05-12 16:55:38 +0530

    Add files via upload

---
+++ jay64.py
@@ -0,0 +1,6 @@
+''' Q3)write a books as to create library class with no- of books and two instance a variable
+  write a program library from this library class and show how you can and get print all books,
+  and a book the number of books wring different method show that your program doesnt perriet the books after the pogram is stopped.'''
+
+
+print("jay kumar singh")
