f75f95174cc710e33143e4773d44b7941d0f7ab9
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-05-16 11:00:22 +0530

    Add files via upload

---
+++ jay76.py
@@ -0,0 +1,8 @@
+# exercise 8
+
+
+# write a program to manipulate pdf files pdf files using pypdf. 
+# your program should be able to merge multiple pdf files into a single pdf.
+
+# You are is a welcome to add more ties 
+# Pypdf functionalities free and open source pure python pdf library capable of splitting merging
