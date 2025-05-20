ec2378b163b9c0dc1173d4b182fc3abb4703ad9c
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-05-20 05:56:04 +0530

    Add files via upload

---
+++ jay85.py
@@ -0,0 +1,6 @@
+# Creating command line Utility:
+
+# Command line utilites are program that can be run from the terminal or command line interface, 
+# and they are an workflow. essential part of many development
+
+# in Python you can create your own command line wilities wing the built in argparie module.
