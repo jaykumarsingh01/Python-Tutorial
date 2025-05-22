6cacf8676c22a5ddf49bff97e8d4b541786178bf
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-05-22 10:47:21 +0530

    Add files via upload

---
+++ jay90.py
@@ -0,0 +1,7 @@
+# news Api in Python
+
+# 2 the news Api and the request module to fetch the daily news related to different topics.
+
+# GO to http./newscapi.org/
+
+# and explore the various options to build you application.
