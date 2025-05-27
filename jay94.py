e9577df5165a56ca40a5cea2910acaea32b088e0
jay kumar singh <jaykumarsingh8117@gmail.com>
2025-05-27 09:35:15 +0530

    Add files via upload

---
+++ jay94.py
@@ -0,0 +1,5 @@
+# exercise 11
+# drink water reminder
+
+# write a pyhton program which remind you of drinking water every hour or two.
+# your program either beep or send desktop notification for a specfic operating system.?
